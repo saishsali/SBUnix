b3a09f6dffad283d3f0e59302f77baa4259c9d7e
saishsali <salisaish@gmail.com>
2017-08-31 15:00:22 -0400

    Read user input

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,6 +1,26 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+
+char *read() {
+    char *input = NULL;
+    size_t len = 1024;
+    ssize_t n;
+
+    if ((n = getline(&input, &len, stdin)) == -1) {
+        exit(EXIT_FAILURE);
+    }
+
+    return input;
+}
 
 int main(int argc, char* argv[]) {
-  puts("sbush> ");
-  return 0;
+    char *input;
+    while (1) {
+        printf("%s", "sbush> ");
+        input = read();
+        puts(input);
+    }
+
+    return 0;
 }
