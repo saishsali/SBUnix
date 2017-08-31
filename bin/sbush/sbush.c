3c6d5417759aa899b4cf06d22a203372891c883b
saishsali <salisaish@gmail.com>
2017-08-31 15:17:28 -0400

    Parse user input

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,10 +1,11 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *read() {
     char *input = NULL;
-    size_t len = 1024;
+    size_t len = 0;
     ssize_t n;
 
     if ((n = getline(&input, &len, stdin)) == -1) {
@@ -14,12 +15,31 @@ char *read() {
     return input;
 }
 
+char **parse(char *input) {
+    int i = 0;
+    char *token;
+    char **tokens = malloc(sizeof(char*) * 1024);
+
+    token = strtok(input, " \t");
+
+    while (token != NULL) {
+        tokens[i++] = token;
+        token = strtok(NULL, " \t");
+    }
+    tokens[i] = NULL;
+
+    return tokens;
+}
+
 int main(int argc, char* argv[]) {
     char *input;
+    char **tokens;
+
     while (1) {
         printf("%s", "sbush> ");
         input = read();
-        puts(input);
+        tokens = parse(input);
+        puts(tokens[1]);
     }
 
     return 0;
