a358835f85b156d41bf1de6b545215d5297f85e3
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-09-01 10:06:28 -0400

    Execute binaries interactively

--- hello.c
+++ hello.c
@@ -9,49 +9,47 @@
 int main ( ) 
 {
     pid_t pid;
-    pid = fork();
-    int i=0, j=0;
-    char *line=NULL;
-    size_t bufsize = 1024;
-    int d;
+    int i = 0, c, j = 0, pos = 0;
     char *path = getenv("PATH");
-    char *line_copy=(char*) malloc(32*sizeof(char));
-    char *envp[] = {path, NULL};
-    char **line1 = (char**) malloc(32*sizeof(char*));
-
+    char *envp[] = { path, NULL };
+    int link[2];
+    if (pipe(link) == -1){
+        exit(1);
+    }
     while(i<5) {
-        j=0;
-        printf(">");
-        d = getline(&line, &bufsize, stdin);
-        strcpy(line_copy,line);
-        char *token = strtok(line, " ");
-
-        while(token!=NULL) {
-            line1[j] = (char*) malloc(32*sizeof(char));
-            strcpy(line1[j++], token);
-            token = strtok(NULL, " ");
-        }
         i++;
+        j=0;
+        pos=0;
+        char *line = (char*) malloc(100*sizeof(char));
+        char **line1 = (char**) malloc(20*sizeof(char*));
+        char *token = (char*) malloc(20*sizeof(char));
+        
+        printf(">");
+        while( ( c = getchar() )!=EOF && c != '\n' ) {
+            line[pos] = c;
+            pos++;
+        }
+        token = strtok(line, " ");
+        while(token != NULL) {
+            line1[j] = token;
+            token = strtok(NULL, " ");
+            j++;
+        }
+        line1[j] = NULL;
+        pid = fork();
         if (pid==0) {
-            if (execvpe(line1[0],line1,envp) == -1) {
-             	printf("some error");
-            }
-             exit(127);
+            execvpe(line1[0], line1, envp);
+            exit(1);
         }
         else {
-             waitpid(pid,0,0);
+            waitpid(pid, 0, 0);
         }
-        
-        FILE *ls = popen(line_copy, "r");
-
-        char buf[256];
-        printf("\n");
-        while (fgets(buf, sizeof(buf), ls) != 0) {
-            printf("%s",buf);
-        }
-        pclose(ls);
-
+        i++;
+        free(line);
+        free(line1);
+        free(token);
     }
+    
 
-    return d;
+    return 0;
 }
