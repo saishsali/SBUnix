95230c2a2e338e94cc2db290b7e93c1639ef5fde
saishsali <salisaish@gmail.com>
2017-09-04 01:29:06 -0400

    Crt implementation

--- crt/crt1.c
+++ crt/crt1.c
@@ -2,4 +2,12 @@
 
 void _start(void) {
   // call main() and exit() here
+    __asm__ (
+        "mov %rsp, %rbp;"
+        "mov 0(%rbp), %rdi;"
+        "mov 8(%rbp), %rsi;"
+        "call main;"
+        "mov $60, %rax;"
+        "syscall;"
+    );
 }
