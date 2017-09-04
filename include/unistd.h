c9ad3a7146ffb09dac41bdfc6ce8f248bdec6416
saishsali <salisaish@gmail.com>
2017-09-04 18:12:39 -0400

    Execve syscall

--- include/unistd.h
+++ include/unistd.h
@@ -14,6 +14,7 @@ char *getcwd(char *buf, size_t size);
 
 pid_t fork();
 int execvpe(const char *file, char *const argv[], char *const envp[]);
+int execvp(const char *file, char *const argv[]);
 pid_t wait(int *status);
 int waitpid(int pid, int *status);
 
