bf0f453fd4b1bd15ac00180e0ad48814cd72a1f4
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-09-04 02:47:34 -0400

    Added ls file

---
+++ bin/ls/ls.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#define BUFSIZE 1024
+
+int main(int argc, char *argv[])
+{
+	char cwd[1024];
+	DIR *curr_dir;
+	struct dirent *current_direct;
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+		curr_dir = opendir (cwd);
+		if (curr_dir == NULL) {
+			printf ("Cannot open directory '%s'\n", cwd);
+			exit(EXIT_FAILURE);
+		}
+
+		while ((current_direct = readdir(curr_dir)) != NULL) {
+			printf ("[%s] ", current_direct->d_name);
+		}
+		printf("\n");
+		closedir (curr_dir);
+	}        
+	else
+		perror("Something went wrong");
+	return 0;
+}
