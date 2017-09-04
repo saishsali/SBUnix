b802e1c2c0a313257779db981f077ff839c2459f
saishsali <salisaish@gmail.com>
2017-09-04 15:00:54 -0400

    Read syscall

---
+++ libc/read.c
@@ -0,0 +1,18 @@
+#include <sys/defs.h>
+
+ssize_t read(int fd, void *buf, size_t count) {
+    ssize_t num_bytes = 12;
+
+    __asm__ (
+        "movq $0, %%rax;"
+        "movq %1, %%rdi;"
+        "movq %2, %%rsi;"
+        "movq %3, %%rdx;"
+        "syscall;"
+        "movq %%rax, %0;"
+        : "=r" (num_bytes)
+        : "r" ((uint64_t)fd), "r" (buf), "r" (count)
+    );
+
+    return num_bytes;
+}
