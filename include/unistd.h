50bb04293984839bd4e2d2ef95ab2624b718f404
saishsali <salisaish@gmail.com>
2017-09-04 01:34:20 -0400

    Implement write using syscall

--- include/unistd.h
+++ include/unistd.h
@@ -14,7 +14,7 @@ char *getcwd(char *buf, size_t size);
 
 pid_t fork();
 int execvpe(const char *file, char *const argv[], char *const envp[]);
-pid_t wait(int *status)
+pid_t wait(int *status);
 int waitpid(int pid, int *status);
 
 unsigned int sleep(unsigned int seconds);
@@ -24,7 +24,7 @@ pid_t getppid(void);
 
 // OPTIONAL: implement for ``on-disk r/w file system (+10 pts)''
 off_t lseek(int fd, off_t offset, int whence);
-int mkdir(const char *pathname, mode_t mode);
+// int mkdir(const char *pathname, mode_t mode);
 
 // OPTIONAL: implement for ``signals and pipes (+10 pts)''
 int pipe(int pipefd[2]);
