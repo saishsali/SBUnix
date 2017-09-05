7a648d8e19a0073d3c14f5c10c63e646d4f0bf58
saishsali <salisaish@gmail.com>
2017-09-05 16:48:38 -0400

    Add string and get env libc

---
+++ libc/getenv.c
@@ -0,0 +1,31 @@
+
+#include <string.h>
+#define BUFSIZE 1024
+
+char *getenv(const char *name, char *const envp[])
+{
+    int key_length = strlen(name);
+    char initial_envp[BUFSIZE], *result = "\0";
+    int i, j;
+
+    if(name == NULL || envp == NULL)
+        return NULL;
+
+    for(i=0; envp[i] != NULL; i++)
+    {
+        strcpy(initial_envp,envp[i]);
+        initial_envp[key_length] = '\0';
+        if(strcmp(name, initial_envp)==0)
+        {
+            for(j=0;envp[i][j]!='\0';j++)
+            {
+                if(envp[i][j] == '=')
+                {
+                    result = envp[i]+j+1;
+                }
+            }
+            break;
+        }
+    }
+    return result;
+}
