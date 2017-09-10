498a4085a5dca80566cda434bcce8cb565177a85
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-09-09 21:46:53 -0400

    krpintf

--- sys/kprintf.c
+++ sys/kprintf.c
@@ -1,5 +1,88 @@
 #include <sys/kprintf.h>
+#include <stdarg.h>
+static char *temp2 = (char*)0xb8000;
+
+void print_char(char value, int color) {
+	if (value == '\n') {
+		int y = (int)((char*)0xb8fa0 - temp2);
+		y = y%160;
+		temp2 = temp2 + y;
+		return;
+	}
+	*temp2 = value;
+	temp2++;
+	*temp2 = color;
+	temp2++;
+}
+
+void print_string(char *arg_value) {
+	while(*arg_value != '\0') {
+		print_char(*arg_value, 7);
+		arg_value++;
+	}
+}
+
+char *convert_value(int decimal, int base) {
+	static char buf[100];
+	char *result = &buf[99];
+	*result = '\0';
+
+	char alphabets[] = "0123456789ABCDEF";
+	int remainder;
+
+	while(decimal > 0) {
+		remainder = decimal % base;
+		decimal /= base;
+		result--;
+		*result = alphabets[remainder];
+	}
+	return result;
+}
 
 void kprintf(const char *fmt, ...)
 {
+	va_list arguments;
+	va_start(arguments, fmt);
+	int i;
+	char c;
+	char *arg_value;
+	const char *iter;
+
+	for(iter = fmt; *iter; iter++) {
+		if (*iter == '%') {
+			iter++;
+
+			switch(*iter) {
+				case 'c':
+					c = va_arg(arguments, int);
+					print_char(c, 7);
+					break;
+				case 'd':
+					i = va_arg(arguments, int);
+					if (i < 0){
+						print_char('-', 7);
+					}
+					arg_value = convert_value(i,10);
+					print_string(arg_value);
+					break;
+				case 'x':
+					i = va_arg(arguments, int);
+					if (i < 0){
+						print_char('-', 7);
+					}
+					arg_value = convert_value(i,16);
+					print_string(arg_value);
+					break;
+				case 's':
+					arg_value = va_arg(arguments, char*);
+					print_string(arg_value);
+					break;
+
+			}
+		} else {
+
+			print_char(*iter, 3);
+		}
+	}
+
 }
--- sys/main.c
+++ sys/main.c
@@ -5,6 +5,7 @@
 #include <sys/ahci.h>
 
 #define INITIAL_STACK_SIZE 4096
+
 uint8_t initial_stack[INITIAL_STACK_SIZE]__attribute__((aligned(16)));
 uint32_t* loader_stack;
 extern char kernmem, physbase;
@@ -18,18 +19,17 @@ void start(uint32_t *modulep, void *physbase, void *physfree)
   while(modulep[0] != 0x9001) modulep += modulep[1]+2;
   for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
     if (smap->type == 1 /* memory */ && smap->length != 0) {
-      kprintf("Available Physical Memory [%p-%p]\n", smap->base, smap->base + smap->length);
+      // kprintf("Available Physical Memory [%p-%p]\n", smap->base, smap->base + smap->length);
     }
   }
-  kprintf("physfree %p\n", (uint64_t)physfree);
-  kprintf("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
+  // kprintf("physfree %p\n", (uint64_t)physfree);
+  // kprintf("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
 }
 
 void boot(void)
 {
   // note: function changes rsp, local stack variables can't be practically used
-  register char *temp1, *temp2;
-
+  register char* temp2;
   for(temp2 = (char*)0xb8001; temp2 < (char*)0xb8000+160*25; temp2 += 2) *temp2 = 7 /* white */;
   __asm__(
     "cli;"
@@ -44,10 +44,23 @@ void boot(void)
     (uint64_t*)&physbase,
     (uint64_t*)(uint64_t)loader_stack[4]
   );
-  for(
-    temp1 = "!!!!! start() returned !!!!!", temp2 = (char*)0xb8000;
-    *temp1;
-    temp1 += 1, temp2 += 2
-  ) *temp2 = *temp1;
+  
+
+  kprintf("Mohit is %d years old\n", 23);
+  kprintf("come");
+  kprintf("come2");
+
+  // for(
+  //   temp1 = "Mohit", temp2 = (char*)0xb8000;
+  //   *temp1;
+  //   temp1 += 1, temp2 += 2
+  // ) *temp2 = *temp1;
+
+  //   for(
+  //   temp1 = "Charu Achi ladki hai";
+  //   *temp1;
+  //   temp1 += 1, temp2 += 2
+  // ) *temp2 = *temp1;
   while(1);
+  
 }
