a469a67019fe77eeea6d322ff56ff6ca5a01f3d7
saishsali <salisaish@gmail.com>
2017-09-14 13:39:19 -0400

    Init idt

--- sys/idt.c
+++ sys/idt.c
@@ -1,5 +1,6 @@
 #include <sys/defs.h>
 #include <sys/gdt.h>
+#define SIZE 256
 
 struct IDT {
     uint16_t offset_1;
@@ -10,3 +11,16 @@ struct IDT {
     uint16_t offset_3;
     uint32_t zero;
 };
+
+typedef struct IDT IDT;
+
+IDT idt[SIZE];
+
+void init_idt() {
+    int i;
+
+    for (i = 0; i < SIZE; i++) {
+        idt[i].ist = 0;
+        idt[i].selector = 8;
+    }
+}
