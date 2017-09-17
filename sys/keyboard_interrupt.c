1a332a925777c21fd9d441243cfa31b249d4125e
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-09-16 20:46:36 -0400

    Control key implement

--- sys/keyboard_interrupt.c
+++ sys/keyboard_interrupt.c
@@ -1,6 +1,11 @@
 #include <sys/kprintf.h>
 #include <sys/io.h>
 #include <sys/keyboard_scancode.h>
+#define initial_pos_y 77
+#define pos_x 24
+#define control_scan_code 29
+static int print_pos = initial_pos_y;
+
 
 void keyboard_interrupt() {
 	int x = inb(0x60);
@@ -8,6 +13,14 @@ void keyboard_interrupt() {
 
 	if (x <= 127) {
 		c = kbdus[inb(0x60)];
-		kprintf_pos(24, 78, "%c", c);
+		if (x == control_scan_code){
+			// Print first ^ at 77 and make next pointer to point at 78
+			kprintf_pos(pos_x, initial_pos_y, "^");
+			print_pos = initial_pos_y + 1;
+		} else {
+			kprintf_pos(pos_x, print_pos, "%c", c);
+			kprintf_pos(pos_x, print_pos + 1, " ");
+			print_pos = initial_pos_y;
+		}
 	}
 }
--- sys/timer_interrupt.c
+++ sys/timer_interrupt.c
@@ -1,10 +1,13 @@
 #include <sys/kprintf.h>
+#define pos_y 74
+#define pos_x 24
+
 int i = 0;
 int timer = 0;
 
 void timer_interrupt() {
     timer++;
     if (timer % 18 == 0) {
-        kprintf_pos(24, 75, "%d", ++i);
+        kprintf_pos(pos_x, pos_y, "%d", ++i);
     }
 }
