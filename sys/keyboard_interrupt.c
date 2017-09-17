98183ec92adfbf6505b458db0ffc7faa12e8481e
saishsali <salisaish@gmail.com>
2017-09-16 20:08:31 -0400

    Handle release scancode

--- sys/keyboard_interrupt.c
+++ sys/keyboard_interrupt.c
@@ -3,15 +3,11 @@
 #include <sys/keyboard_scancode.h>
 
 void keyboard_interrupt() {
-	static int i = 0;
-	int c;
+	int x = inb(0x60);
+	char c;
 
-	if (i == 0) {
+	if (x <= 127) {
 		c = kbdus[inb(0x60)];
 		kprintf_pos(24, 78, "%c", c);
-		i = 1;
-	} else {
-		inb(0x60);
-		i = 0;
 	}
 }
