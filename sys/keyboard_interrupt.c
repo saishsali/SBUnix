0ee894e8cd4631a23c6e1911f56556815bc59e8a
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-09-16 20:49:31 -0400

    Changed macros name

--- sys/keyboard_interrupt.c
+++ sys/keyboard_interrupt.c
@@ -1,10 +1,10 @@
 #include <sys/kprintf.h>
 #include <sys/io.h>
 #include <sys/keyboard_scancode.h>
-#define initial_pos_y 77
-#define pos_x 24
-#define control_scan_code 29
-static int print_pos = initial_pos_y;
+#define INITIAL_POS_Y 77
+#define POS_X 24
+#define CONTROL_SCAN_CODE 29
+static int print_pos = INITIAL_POS_Y;
 
 void keyboard_interrupt() {
 	int x = inb(0x60);
@@ -12,14 +12,14 @@ void keyboard_interrupt() {
 
 	if (x <= 127) {
 		c = kbdus[inb(0x60)];
-		if (x == control_scan_code){
+		if (x == CONTROL_SCAN_CODE){
 			// Print first ^ at 77 and make next pointer to point at 78
-			kprintf_pos(pos_x, initial_pos_y, "^");
-			print_pos = initial_pos_y + 1;
+			kprintf_pos(POS_X, INITIAL_POS_Y, "^");
+			print_pos = INITIAL_POS_Y + 1;
 		} else {
-			kprintf_pos(pos_x, print_pos, "%c", c);
-			kprintf_pos(pos_x, print_pos + 1, " ");
-			print_pos = initial_pos_y;
+			kprintf_pos(POS_X, print_pos, "%c", c);
+			kprintf_pos(POS_X, print_pos + 1, " ");
+			print_pos = INITIAL_POS_Y;
 		}
 	}
 }
