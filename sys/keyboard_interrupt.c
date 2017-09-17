b40d90fcb929becef8035c0caa63e1cc72679547
saishsali <salisaish@gmail.com>
2017-09-16 21:11:36 -0400

    Back merge control key handling

--- sys/keyboard_interrupt.c
+++ sys/keyboard_interrupt.c
@@ -9,17 +9,32 @@ static int print_pos = INITIAL_POS_Y;
 void keyboard_interrupt() {
 	int x = inb(0x60);
 	char c;
+	static int shift = 0;
 
-	if (x <= 127) {
-		c = kbdus[inb(0x60)];
-		if (x == CONTROL_SCAN_CODE){
-			// Print first ^ at 77 and make next pointer to point at 78
+	// Scancode of pressed key
+	if (x <= 0x7F) {
+
+		// Shift key
+		if (x == 0x2A || x == 0x36) {
+			shift = 1;
+
+			return;
+		} else if (x == CONTROL_SCAN_CODE) {
 			kprintf_pos(POS_X, INITIAL_POS_Y, "^");
 			print_pos = INITIAL_POS_Y + 1;
-		} else {
-			kprintf_pos(POS_X, print_pos, "%c", c);
-			kprintf_pos(POS_X, print_pos + 1, " ");
-			print_pos = INITIAL_POS_Y;
+
+			return;
 		}
+
+		if (shift == 1) {
+			c = shift_mappings[x];
+			shift = 0;
+		} else {
+			c = kbdus[x];
+		}
+		// kprintf_pos(24, 78, "%c", c);
+		kprintf_pos(POS_X, print_pos, "%c", c);
+		kprintf_pos(POS_X, print_pos + 1, " ");
+		print_pos = INITIAL_POS_Y;
 	}
 }
