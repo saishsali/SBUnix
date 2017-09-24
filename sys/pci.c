5d97ccb06ea241cfdfaea272222376996cace359
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-09-24 13:07:59 -0400

    check

--- sys/pci.c
+++ sys/pci.c
@@ -12,7 +12,7 @@
 #define SATA_SIG_PM             0x96690101  // Port multiplier
 #define HBA_PORT_DET_PRESENT    3
 #define HBA_PORT_IPM_ACTIVE     1
-#define BAR_MEM                 0x20000000
+#define BAR_MEM                 0x30000000
 
 // Check device type
 int check_type(hba_port_t *port)
@@ -109,19 +109,19 @@ uint64_t pci_read_bar(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
     return (tmp);
 }
 
-uint32_t pci_write_bar(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
-    uint32_t address;
-    uint32_t lbus  = (uint32_t)bus;
-    uint32_t lslot = (uint32_t)slot;
-    uint32_t lfunc = (uint32_t)func;
-    uint32_t tmp = 0;
+// uint32_t pci_write_bar(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
+//     uint32_t address;
+//     uint32_t lbus  = (uint32_t)bus;
+//     uint32_t lslot = (uint32_t)slot;
+//     uint32_t lfunc = (uint32_t)func;
+//     uint32_t tmp = 0;
 
-    address = (uint32_t)((lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xfc) | ((uint32_t)0x80000000));
-    outl(0xCF8, address);
-    outl(0xCFC, BAR_MEM);
-    tmp = (uint32_t)(inl(0xCFC));
-    return (tmp);
-}
+//     address = (uint32_t)((lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xfc) | ((uint32_t)0x80000000));
+//     outl(0xCF8, address);
+//     outl(0xCFC, BAR_MEM);
+//     tmp = (uint32_t)(inl(0xCFC));
+//     return (tmp);
+// }
 
 void device_info(uint8_t bus, uint8_t device) {
     uint16_t vendor_id, device_id, class_subclass;
@@ -136,7 +136,8 @@ void device_info(uint8_t bus, uint8_t device) {
             kprintf("Vendor ID: %x, Device ID: %x\n", vendor_id, device_id);
             bar5 = pci_read_bar(bus, device, 0 , 0x24);
             kprintf("Bar5: %x\n", bar5);
-            bar5 = pci_write_bar(bus, device, 0 , 0x24);
+            outl(0xCFC, BAR_MEM);
+            bar5 = (uint32_t)(inl(0xCFC));
             kprintf("Bar5: %x\n", bar5);
             probe_port((hba_mem_t *)((uint64_t)(0xffffffff80000000 + BAR_MEM)));
         }
