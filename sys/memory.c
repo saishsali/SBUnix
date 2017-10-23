2d14f8dffbf4b80c0b5ebf7a713e8ce8f053d436
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-10-23 00:04:28 -0400

    added freelist memory space

--- sys/memory.c
+++ sys/memory.c
@@ -26,29 +26,14 @@ void create_page_descriptor(uint32_t *modulep, void *physbase, void *physfree) {
 
     // uint64_t *address = NULL;
     struct page_descriptor *freelist=NULL;
-    struct page_descriptor *start_freelist=NULL;
-    // int i;
-    int flag=0;
-    kprintf("\n num pages - %d", PAGE_SIZE);
+    // struct page_descriptor *start_freelist=NULL;
 
     for(smap = (struct smap_t*)(modulep + 2); smap < (struct smap_t*)((char*)modulep+modulep[1] + 2 * 4); ++smap) {
         if (smap->type == 1 /* memory */ && smap->length != 0) {
-            num_pages = smap->length/PAGE_SIZE;
-            if(flag == 0) {
-                freelist = mmap (&smap->base, PAGE_SIZE, PROT_WRITE , MAP_ANONYMOUS, 0, 0);
-                start_freelist = freelist;
-                flag = 1;
-            }
-            kprintf("\n here");
-            
-            // for(i = 1; i<num_pages; i++) {
-            //     address = &smap->base + PAGE_SIZE*i;
-            //     freelist->next = mmap (address, PAGE_SIZE, PROT_WRITE | PROT_READ , MAP_FIXED | MAP_ANONYMOUS, 0, 0);
-            //     freelist = freelist->next;
-            // }
-            kprintf("\n pages - %d", smap->length/PAGE_SIZE);
+            num_pages += smap->length/PAGE_SIZE;
         }
     }
-    kprintf("\n pages list - %p %p", start_freelist, freelist);
-    kprintf("Number of pages: %d\n", num_pages);
+
+    freelist = mmap (physfree, sizeof(struct page_descriptor) * num_pages, PROT_WRITE|PROT_READ , MAP_ANONYMOUS, 0, 0);
+    kprintf("Number of pages: %d %p\n", num_pages, freelist);
 }
