117de5d7ceeb6f3aa6502276259906d7acebb083
saishsali <salisaish@gmail.com>
2017-10-27 17:21:44 -0400

    Allocate page

--- include/sys/page_descriptor.h
+++ include/sys/page_descriptor.h
@@ -2,17 +2,19 @@
 #define _MEMORY_H
 
 /* Page size is 4KB */
-#define PAGE_SIZE 4096
+#define PAGE_SIZE  4096
+#define PAGE_SHIFT 12
 
 struct Page {
     /* Next page on free list */
     struct Page *next;
-    /* Count of pointers referring to this page */
+    /* Track the number of active mappings */
     uint16_t reference_count;
 }__attribute__((packed));
 
 typedef struct Page Page;
 
 void page_init(uint64_t start, uint64_t end, uint64_t physbase, uint64_t physfree);
+void *allocate_page();
 
 #endif
--- sys/page_descriptor.c
+++ sys/page_descriptor.c
@@ -1,13 +1,18 @@
 #include <sys/kprintf.h>
 #include <sys/defs.h>
 #include <sys/page_descriptor.h>
+#include <sys/kprintf.h>
 
-Page *page_free_list;
+Page *page_free_list, *pages;
+
+uint64_t page_to_physical_address(Page *p) {
+    return (p - pages) << PAGE_SHIFT;
+}
 
 void page_init(uint64_t start, uint64_t end, uint64_t physbase, uint64_t physfree) {
     static uint64_t index = 0;
     /* Start page descriptor array at physfree */
-    Page *page = (Page *)physfree;
+    pages = (Page *)physfree;
     Page *prev = NULL;
     uint64_t i = 0;
     start = start / PAGE_SIZE;
@@ -15,34 +20,46 @@ void page_init(uint64_t start, uint64_t end, uint64_t physbase, uint64_t physfre
 
     /* Mark page index to start as in use so that it can never be allocated */
     for (i = index; i < start; i++) {
-        page[i].reference_count = 1;
-        page[i].next = NULL;
+        pages[i].reference_count = 1;
+        pages[i].next = NULL;
     }
 
     /* Mark page 0 as in use */
     if (start == 0) {
-        page[0].reference_count = 1;
-        page[0].next = NULL;
+        pages[0].reference_count = 1;
+        pages[0].next = NULL;
         start += 1;
     }
 
     for (i = start; i < end; i++) {
         /* Mark kernel memory as in use */
         if (i >= (physbase / PAGE_SIZE) && i < (physfree / PAGE_SIZE)) {
-            page[i].reference_count = 1;
-            page[i].next = NULL;
+            pages[i].reference_count = 1;
+            pages[i].next = NULL;
         } else {
-            /* Mark rest of the memory as free */
-            page[i].reference_count = 0;
-            page[i].next = NULL;
+            /* Mark rest of the memory pages as free */
+            pages[i].reference_count = 0;
+            pages[i].next = NULL;
             if (prev != NULL) {
-                prev->next = &page[i];
+                prev->next = &pages[i];
             }
             if (page_free_list == NULL) {
-                page_free_list = &page[i];
+                page_free_list = &pages[i];
             }
-            prev = &page[i];
+            prev = &pages[i];
         }
     }
     index = end;
+    // To do: Mark pages used by page descriptor array as in use
+}
+
+void *allocate_page() {
+    if (page_free_list == NULL) {
+        kprintf("No free pages for allocation\n");
+        return NULL;
+    }
+    Page *free_page = page_free_list;
+    page_free_list = page_free_list->next;
+
+    return free_page;
 }
