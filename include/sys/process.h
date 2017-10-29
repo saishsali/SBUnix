eb2b99c9d06426ebea165ad1caa38848335298f3
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-10-29 00:09:29 -0400

    Structure for pcb

---
+++ include/sys/process.h
@@ -0,0 +1,12 @@
+#ifndef _PROCESS_H
+#define _PROCESS_H
+
+struct PCB {
+   char kstack[4096];
+   uint64_t pid;
+   uint64_t rsp;
+   enum { RUNNING, SLEEPING, ZOMBIE } state;
+   int exit_status;
+};
+
+#endif
