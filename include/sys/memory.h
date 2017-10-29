49f01c4262c17ccb8e9bed00592bba666ce33db7
saishsali <salisaish@gmail.com>
2017-10-29 01:17:05 -0400

    Add new line and remove unnecessary files

--- include/sys/memory.h
+++ include/sys/memory.h
@@ -1,7 +1,6 @@
 #ifndef _MEMORY_H
 #define _MEMORY_H
 
-
 void *kmalloc(size_t size, int flags);
 
 #endif
--- include/sys/mmap.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MMAP_H
-#define _MMAP_H
-
-void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
-
-#endif
