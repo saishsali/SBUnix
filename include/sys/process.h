880f83aeedd4141c92a8d57d9ab26f9d7ce1199a
saishsali <salisaish@gmail.com>
2017-10-30 03:48:34 -0400

    Indent process struct

--- include/sys/process.h
+++ include/sys/process.h
@@ -2,11 +2,11 @@
 #define _PROCESS_H
 
 struct PCB {
-   char kstack[4096];
-   uint64_t pid;
-   uint64_t rsp;
-   enum { RUNNING, SLEEPING, ZOMBIE } state;
-   int exit_status;
+    char kstack[4096];
+    uint64_t pid;
+    uint64_t rsp;
+    enum { RUNNING, SLEEPING, ZOMBIE } state;
+    int exit_status;
 };
 
 #endif
