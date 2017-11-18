ed3ec03d8a2fd39e90dcad2a399bc9093b349f6b
saishsali <salisaish@gmail.com>
2017-11-18 14:12:26 -0500

    Zero out pages in allocate page

--- sys/memory.c
+++ sys/memory.c
@@ -2,24 +2,16 @@
 #include <sys/page_descriptor.h>
 #include <sys/paging.h>
 #include <sys/string.h>
+#include <sys/process.h>
 
 void *kmalloc(size_t size) {
-    uint64_t start_address, virtual_address, num_pages;
+    uint64_t num_pages;
 	Page *p = NULL;
 
     num_pages = (ROUND_UP(size, PAGE_SIZE)) / PAGE_SIZE;
 	p = allocate_pages(num_pages);
-    start_address = page_to_virtual_address(p);
-    virtual_address = start_address;
 
-    while (num_pages--) {
-        // Pages are already mapped in page tables
-        memset((void *)virtual_address, 0, PAGE_SIZE);
-        p = p->next;
-        virtual_address = page_to_virtual_address(p);
-    }
-
-	return (void*) start_address;
+	return (void*) page_to_virtual_address(p);
 }
 
 void *kmalloc_user(size_t size) {
@@ -33,7 +25,6 @@ void *kmalloc_user(size_t size) {
 
     while (num_pages--) {
         map_page(virtual_address, page_to_physical_address(p));
-        memset((void *)virtual_address, 0, PAGE_SIZE);
         p = p->next;
         virtual_address += PAGE_SIZE;
     }
@@ -51,10 +42,30 @@ void *kmalloc_map(size_t size, uint64_t virtual_address) {
 
     while (num_pages--) {
         map_page(virtual_address, page_to_physical_address(p));
-        memset((void *)virtual_address, 0, PAGE_SIZE);
         p = p->next;
         virtual_address += PAGE_SIZE;
     }
 
     return (void*) start_address;
+}
+
+/* Validate if the address (upto size bytes) is not used by traversing vma list for a task */
+int validate_address(task_struct *task, uint64_t address, uint64_t size) {
+    if (address >= KERNBASE) {
+        return 0;
+    }
+    vma_struct *vma = task->mm->head;
+
+    while (vma != NULL) {
+        if (address <= vma->start && (address + size) > vma->start) {
+            return 0;
+        } else if (address < vma->end && (address + size) >= vma->end) {
+            return 0;
+        } else if (address > vma->start && (address + size) < vma->end) {
+            return 0;
+        }
+        vma = vma->next;
+    }
+
+    return 1;
 }
