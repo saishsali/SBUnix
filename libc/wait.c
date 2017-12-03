2be66cad81adf2dfd0da09b31a7d07f053a5a5c5
saishsali <salisaish@gmail.com>
2017-12-03 03:12:25 -0500

    Wait libc

---
+++ libc/wait.c
@@ -0,0 +1,17 @@
+#include <sys/defs.h>
+
+pid_t wait(int *status) {
+    int64_t pid;
+
+    __asm__ (
+        "movq $61, %%rax;"
+        "movq %1, %%rdi;"
+        "int $0x80;"
+        "movq %%rax, %0;"
+        : "=r" (pid)
+        : "r" (status)
+        : "%rax", "%rdi"
+    );
+
+    return pid;
+}
