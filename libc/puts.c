da70b9b405160717a9a27b657dcebb1e66d675f8
saishsali <salisaish@gmail.com>
2017-12-03 14:56:40 -0500

    Change indentation in puts

--- libc/puts.c
+++ libc/puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 int puts(const char *s)
 {
-  for( ; *s; ++s) if (putchar(*s) != *s) return EOF;
-  return (putchar('\n') == '\n') ? 0 : EOF;
+    for( ; *s != '\0'; ++s) if (putchar(*s) != *s) return EOF;
+    return (putchar('\n') == '\n') ? 0 : EOF;
 }
