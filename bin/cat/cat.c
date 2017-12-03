117101a18fbb58e27c1a36e8eec11f569ad32a48
saishsali <salisaish@gmail.com>
2017-12-03 18:09:25 -0500

    Working cat

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -2,41 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/dirent.h>
 #define BUFSIZE 1024
 
 int main(int argc, char *argv[], char *envp[]) {
-    // int i, fd;
-    // char input[BUFSIZE], c;
-    // ssize_t n;
+    int i, fd;
+    char input[BUFSIZE], c;
+    ssize_t n;
 
-    // if (argc > 1) {
-    //     for (i = 1; i < argc; i++) {
-    //         fd = open(argv[i], 0x0000);
-    //         if (fd < 0)
-    //             exit(0);
-    //         while ((n = read(fd, &c, 1) != 0))
-    //             putchar(c);
+    if (argc > 1) {
+        for (i = 1; i < argc; i++) {
+            fd = open(argv[i], O_RDONLY);
+            if (fd < 0)
+                exit(1);
+            while ((n = read(fd, &c, 1) != 0)) {
+                putchar(c);
+            }
 
-    //         close(fd);
-    //     }
-    // } else {
-    //     while ((n = read(0, input, BUFSIZE)) != -1) {
-    //         i = 0;
-    //         while (input[i] != '\0') {
-    //             putchar(input[i]);
-    //             input[i++] = '\0';
-    //         }
-    //     }
-    // }
-    putchar(argc + 48);
-    puts(argv[0]);
-    puts(argv[1]);
-    puts("Hello man");
-
+            close(fd);
+        }
+    } else {
+        while ((n = read(0, input, BUFSIZE)) != -1) {
+            i = 0;
+            while (input[i] != '\0') {
+                putchar(input[i]);
+                input[i++] = '\0';
+            }
+        }
+    }
     exit(1);
-
-    // sys_exit();
-    while(1);
-
-    return 0;
 }
