380c543ff17699535a79ce6a59c0c2ccfd1c2094
Your Name <you@example.com>
2017-12-06 17:43:53 -0500

    move unlink to OPTIONAL section

--- include/unistd.h
+++ include/unistd.h
@@ -7,7 +7,6 @@ int open(const char *pathname, int flags);
 int close(int fd);
 ssize_t read(int fd, void *buf, size_t count);
 ssize_t write(int fd, const void *buf, size_t count);
-int unlink(const char *pathname);
 
 int chdir(const char *path);
 char *getcwd(char *buf, size_t size);
@@ -25,6 +24,7 @@ pid_t getppid(void);
 // OPTIONAL: implement for ``on-disk r/w file system (+10 pts)''
 off_t lseek(int fd, off_t offset, int whence);
 int mkdir(const char *pathname, mode_t mode);
+int unlink(const char *pathname);
 
 // OPTIONAL: implement for ``signals and pipes (+10 pts)''
 int pipe(int pipefd[2]);
