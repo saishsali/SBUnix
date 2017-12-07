a777a3a0b3b3c41d720b7a94b0057d8f73d76897
Charu Mehndiratta <cmehndiratta@cs.stonybrook.edu>
2017-12-07 12:55:58 -0500

    Init rc proces and modified reading script code

--- bin/init/init.c
+++ bin/init/init.c
@@ -2,14 +2,29 @@
 #include <unistd.h>
 #include <sys/defs.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[], char *envp[]) {
     pid_t pid;
     int status;
+    char *new_argv[] = {"/rootfs/bin/sbush", "/rootfs/etc/rc", NULL};
+
+    pid = fork();
+    if(pid == 0) {
+        if (execvpe("/rootfs/bin/sbush", new_argv, envp) < 0) {
+            printf("-sbush: command not found");
+            exit(1);
+        }
+    } else {
+        waitpid(pid, NULL);
+    }
 
     pid = fork();
     if (pid == 0) {
-        execvpe("/rootfs/bin/sbush", NULL, envp);
+        if (execvpe("/rootfs/bin/sbush", NULL, envp) < 0) {
+            printf("-sbush: command not found");
+            exit(1);
+        }
     } else {
         while (1) {
             if ((int)wait(&status) < 0) {
