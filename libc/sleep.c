ef52be8eb939265c528f95e13fd5e8155f274f7d
saishsali <salisaish@gmail.com>
2017-12-07 20:56:36 -0500

    Remove commented code

--- libc/sleep.c
+++ libc/sleep.c
@@ -1,32 +1,5 @@
 #include <sys/defs.h>
 
-// unsigned int sleep(unsigned int seconds) {
-//     uint32_t status;
-
-//     struct time {
-//        long tv_sec;
-//        long tv_nsec;
-//     };
-
-//     struct time req;
-//     struct time rem;
-//     req.tv_sec = seconds;
-//     req.tv_nsec = 0;
-
-//     __asm__ (
-//         "movq $35, %%rax;"
-//         "movq %1, %%rdi;"
-//         "movq %2, %%rsi;"
-//         "syscall;"
-//         "movq %%rax, %0;"
-//         : "=r" (status)
-//         : "r" (&req), "r" (&rem)
-//     );
-
-//     return status;
-// }
-
-
 uint32_t sleep(uint64_t seconds) {
     uint64_t result;
     __asm__ (
